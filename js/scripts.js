dcd7114bb951fccb25d720cc5a6f842a5ca0599e
Naresh Aithagoni <naithagoni@hms-dev.com>
2020-01-06 19:36:28 +0100

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// $(document).ready(() => {
+//     setTimeout(() => {
+//         var firstnameLength = $(".firstname").length;
+//         $($(".firstname").get()).each((i, e) => {
+//             $(e).delay(500 * (i - 1)).fadeIn(500);
+//             if(i >= firstnameLength - 1) {
+//                 $(e).animate({
+//                     "margin-right": "25px"
+//                 }, 500);
+//                 $(".info").delay(700 * i).fadeIn(1500);
+//             }
+//         });
+//     }, 1000);
+// });
